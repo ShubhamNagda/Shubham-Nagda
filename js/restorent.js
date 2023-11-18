2fac0ec090165fa0869295e140c1c2e9ff66a540
Shubham Nagda <119925058+ShubhamNagda@users.noreply.github.com>
2023-11-18 13:54:54 +0530

    Add files via upload

---
+++ js/restorent.js
@@ -0,0 +1,34 @@
+var a = "thanks for buy Rasgulla";
+var b = "thanks for buy Gulab Jamun";
+var c = "thanks for buy Ras Malai";
+var d = "thanks for buy Jalebi";
+var e = "thanks for buy Laddu";
+var f = "thanks for buy Shrikhand";
+var g = "thanks for buy Lyangcha";
+var h = "thanks for buy Marzipan";
+var i = "thanks for buy CakeIce Cream Chocolate";
+var j = "thanks for buy Cupcake";
+var k = "thanks for buy doughnut";
+var l = "thanks for buy Popcorn";
+var m = "thanks for buy Momo";
+var n= "thanks for buy Pasta";
+var o = "thanks for buy Bread";
+var p = "thanks for buy Crackers";
+var q = "thanks for buy Bread stick";
+var r = "thanks for buy Spring Rolls";
+var s = "thanks for buy Poha";
+var t= "thanks for buy Pizza";
+var u = "thanks for buy Burger";
+var v = "thanks for buy Dough";
+var w = "thanks for buy French Fries ";
+var x = "thanks for buy Hot Dog";
+var y = "thanks for buy Snadwich";
+var z = "thanks for buy Onion Rings";
+var za = "thanks for buy Noodles ";
+var zb = "thanks for buy Samosa";
+var zc = "thanks for buy Rice";
+var zd = "thanks for buy Korma";
+var ze = "thanks for buy Dhokla";
+function buy(x) {
+  alert("This order will be dispatched to your address within 30 minutes \n" + x)
+}
